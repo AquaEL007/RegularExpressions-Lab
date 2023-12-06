5e5efb1db40853b562ff3ea6a19e0c3331b438f0
AquaEL007 <gvgenov@gmail.com>
2023-12-06 16:23:28 +0200

    Add project files.

---
+++ 02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359)( |-)(2)\2(\d{3})\2(\d{4}\b)";
+            string input = Console.ReadLine();
+
+            MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", matchCollection));
+        }
+    }
+}
---
+++ 03.MatchDates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<day>\d{2})(?<separator>\.|-|/)(?<month>[A-Z][a-z]{2})\<separator>(?<year>\d{4})";
+            string input = Console.ReadLine();
+
+            MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+            foreach (Match match in matchCollection)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}
